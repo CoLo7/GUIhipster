0524b7904269f7125c24af1d3746958313c2220d
CoLo7 <gabeaguilar7@gmail.com>
2015-11-19 08:24:09 -0700

    Set up Books complete

--- src/hipster/model/Hipster.java
+++ src/hipster/model/Hipster.java
@@ -4,13 +4,15 @@ public class Hipster
 {
 	private String name;
 	private String [] hipsterPhrases;
+	private Book [] hipsterBooks;
 	
 	public Hipster()
 	{
 		this.name = "";
 		this.hipsterPhrases = new String[4];
-		
+		this.hipsterBooks = new Book[3];
 		setupArray();
+		setupBooks();
 	}
 	
 	private void setupArray()
@@ -21,9 +23,46 @@ public class Hipster
 		hipsterPhrases[3] = "I was into some before it was cool";
 	}
 	
+	private void setupBooks()
+	{
+		Book firstBook, secondBook, thirdBook;
+		
+		firstBook = new Book();
+		firstBook.setAuthor("Neal Stephenson");
+		firstBook.setTitle("Diamond Age");
+		firstBook.setSubject("Science Fiction");
+		firstBook.setPageCount(405);
+		firstBook.setPrice(24.99);
+		
+		secondBook = new Book();
+		secondBook.setAuthor("Neal Stephenson");
+		secondBook.setTitle("Diamond Age");
+		secondBook.setSubject("Science Fiction");
+		secondBook.setPageCount(405);
+		secondBook.setPrice(24.99);
+		
+		thirdBook = new Book(457, "John Doe", "The Subject", "That One Book", 32.99);
+		
+		hipsterBooks[0] = firstBook;
+		hipsterBooks[1] = secondBook;
+		hipsterBooks[2] = thirdBook;		
+	}
+	
 	public String getName() 
 	{
 		return name;
+	}
+
+	public String[] getHipsterPhrases() {
+		return hipsterPhrases;
+	}
+
+	public Book[] getHipsterBooks() {
+		return hipsterBooks;
+	}
+
+	public void setHipsterBooks(Book[] hipsterBooks) {
+		this.hipsterBooks = hipsterBooks;
 	}
 
 	public void setName(String name) {
@@ -31,10 +70,6 @@ public class Hipster
 	}
 	
 	
-	public String[] getHipsterPhrases() {
-		return hipsterPhrases;
-	}
-
 	public void setHipsterPhrases(String[] hipsterPhrases) {
 		this.hipsterPhrases = hipsterPhrases;
 	}
